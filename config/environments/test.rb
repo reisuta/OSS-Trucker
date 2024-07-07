44da5225dfc89a00573cb05f46ae7ee73df719c4
reisuta <in345vain@gmail.com>
2024-07-07 12:10:49 +0900

    :sparkles: CI上でメールを送信するフローの構築

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -42,7 +42,6 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = { address: 'mailhog', port: 1025 }
   config.action_mailer.smtp_settings = {
     address: Rails.application.credentials.dig(:smtp, :address),
     port: Rails.application.credentials.dig(:smtp, :port),
@@ -50,7 +49,6 @@ Rails.application.configure do
     password: Rails.application.credentials.dig(:smtp, :password),
     authentication: Rails.application.credentials.dig(:smtp, :authentication)
   }
-
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
@@ -60,10 +58,4 @@ Rails.application.configure do
 
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
-
-  # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
-
-  # Annotate rendered view with file names.
-  # config.action_view.annotate_rendered_view_with_filenames = true
 end
